fc291cca28e92dc4b13add8f893e43aa3c604b87
RagnarOige <ragnaroige@gmail.com>
2026-01-30 11:05:06 +0200

    Laravel

---
+++ app/Console/Commands/TimetableNotification.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\Timetable;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Mail;
+
+class TimetableNotification extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:timetable-notification';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+
+    $startOfWeek = now()->addWeek()->startOfWeek();
+    $endOfWeek = now()->addWeek()->endOfWeek();
+
+        $cachedResponse = Cache::remember($startOfWeek->toDateString(), now()->addHour(), function() use ($startOfWeek, $endOfWeek) {
+            return Http::get('https://tahveltp.edu.ee/hois_back/timetableevents/timetableSearch', [
+                'from' => $startOfWeek->toIso8601String(),
+                'lang' => 'ET',
+                'page' => '0',
+                'schoolId' => '38',
+                'size' => '50',
+                'studentGroups' => '4b26d1e5-11ac-4c63-840e-46c450c529ee',
+                'thru' =>  $endOfWeek->toIso8601String(),
+            ])->json();
+        });
+
+        $content = data_get($cachedResponse, 'content', []);
+
+        $items = [];
+
+        foreach ($content as $item) {
+
+            $date = Carbon::parse(data_get($item, 'date'))->locale('et');
+            $items[$date->dayName][] = [
+                'name' => data_get($item, 'nameEt'),
+                'date' => $date->translatedFormat('d. F Y'),
+                'start' => data_get($item, 'timeStart'), 
+                'end' => data_get($item, 'timeEnd'), 
+                'room' => data_get($item, 'rooms.0.roomCode'), 
+                ];
+        }
+
+        Mail::to('example@example.com')
+            ->send(new Timetable($items, $startOfWeek->translatedFormat('d. F Y'), $endOfWeek->translatedFormat('d. F Y')));
+
+        foreach ($items as $day => $lessons) {
+            $this->info($day);
+            $this->table(
+                ['nimetus', 'kuupaev', 'algus', 'lopp', 'klass'],
+                $lessons,
+            );
+        }
+    }
+}
---
+++ app/Mail/Timetable.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class Timetable extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(
+        public array $days,
+        public string $startOfWeek,
+        public string $endOfWeek
+    )
+    {
+        
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: sprintf('Nadala Tunniplaan %s - %s', $this->startOfWeek, $this->endOfWeek),
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'mail.timetable',
+            with: [
+                'days' => $this->days
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
