088590f2acd3843876089a97124f6c698a851360
RagnarOige <ragnaroige@gmail.com>
2026-02-02 11:00:11 +0200

    la

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
+use App\Console\Commands\TimetableNotification;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
+    ->withSchedule(function (Schedule $schedule) {
+    $schedule->call(new TimetableNotification)->everyOneMinutes();
+    })
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
